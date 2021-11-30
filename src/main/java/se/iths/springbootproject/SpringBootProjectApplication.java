cd418a3c3eb45ae8a3bcfd7feff607eb18a59877
SpicyIRamen <spicyiramen@gmail.com>
2021-11-30 14:28:57 +0100

    Initial commit

---
+++ src/main/java/se/iths/springbootproject/SpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package se.iths.springbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProjectApplication.class, args);
+    }
+
+}
