347ca6eaad20308befadceaa3286c6c9ffe534b0
Ola Amberntsson <ola.amberntsson@iths.se>
2021-12-01 11:51:17 +0100

    added CharacterService and CharacterController

--- src/main/java/se/iths/springbootproject/controller/CharacterController.java
+++ src/main/java/se/iths/springbootproject/controller/CharacterController.java
@@ -1,4 +1,46 @@
 package se.iths.springbootproject.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import se.iths.springbootproject.entity.AccountEntity;
+import se.iths.springbootproject.entity.CharacterEntity;
+import se.iths.springbootproject.service.CharacterService;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("characters")
 public class CharacterController {
+
+    private final CharacterService characterService;
+
+    public CharacterController(CharacterService characterService) {
+        this.characterService = characterService;
+    }
+
+    @PostMapping
+    public ResponseEntity<CharacterEntity> createCharacter(@RequestBody CharacterEntity characterEntity)
+    {
+        CharacterEntity createdCharacter = characterService.createCharacter(characterEntity);
+        return new ResponseEntity<>(createdCharacter, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteCharacter(@PathVariable Long id) {
+        characterService.deleteCharacter(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Optional<CharacterEntity>> findCharacterById(@PathVariable Long id) {
+        Optional<CharacterEntity> foundCharacter = characterService.findCharacterById(id);
+        return new ResponseEntity<>(foundCharacter, HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public ResponseEntity<Iterable<CharacterEntity>> findAllCharacters() {
+        Iterable<CharacterEntity> allCharacters = characterService.findAllCharacters();
+        return new ResponseEntity<>(allCharacters, HttpStatus.OK);
+    }
 }
---
+++ src/main/java/se/iths/springbootproject/service/CharacterService.java
@@ -0,0 +1,36 @@
+package se.iths.springbootproject.service;
+
+import org.springframework.stereotype.Service;
+import se.iths.springbootproject.entity.AccountEntity;
+import se.iths.springbootproject.entity.CharacterEntity;
+import se.iths.springbootproject.repository.CharacterRepo;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
+
+@Service
+public class CharacterService {
+
+    private final CharacterRepo characterRepo;
+
+    public CharacterService(CharacterRepo characterRepo) {
+        this.characterRepo = characterRepo;
+    }
+
+    public CharacterEntity createCharacter (CharacterEntity characterEntity){
+        return characterRepo.save(characterEntity);
+    }
+
+    public void deleteCharacter (Long characterId){
+        CharacterEntity foundCharacter = characterRepo.findById(characterId).orElseThrow(EntityNotFoundException::new);
+        characterRepo.deleteById(foundCharacter.getCharacterid());
+    }
+
+    public Optional<CharacterEntity> findCharacterById(Long characterId) {
+        return characterRepo.findById(characterId);
+    }
+
+    public Iterable<CharacterEntity> findAllCharacters() {
+        return characterRepo.findAll();
+    }
+}
