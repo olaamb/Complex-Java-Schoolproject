95b914a5280cc6c18edab5d45dc6ff9543c95276
Ola Amberntsson <ola.amberntsson@iths.se>
2021-12-01 14:27:23 +0100

    added ResourcesController and ResourcesService

--- src/main/java/se/iths/springbootproject/controller/ResourcesController.java
+++ src/main/java/se/iths/springbootproject/controller/ResourcesController.java
@@ -1,4 +1,48 @@
 package se.iths.springbootproject.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import se.iths.springbootproject.entity.ResourcesEntity;
+import se.iths.springbootproject.service.ResourcesService;
+
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("resources")
 public class ResourcesController {
+
+    private final ResourcesService resourcesService;
+
+    public ResourcesController(ResourcesService resourcesService)
+    {
+        this.resourcesService = resourcesService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ResourcesEntity> createResources(@RequestBody ResourcesEntity resourcesEntity)
+    {
+        ResourcesEntity createdResources = resourcesService.createResources(resourcesEntity);
+        return new ResponseEntity<>(createdResources, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteResources(@PathVariable Long id) {
+        resourcesService.deleteResources(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Optional<ResourcesEntity>> findResourcesById(@PathVariable Long id) {
+        Optional<ResourcesEntity> foundResources = resourcesService.findResourcesById(id);
+        return new ResponseEntity<>(foundResources, HttpStatus.OK);
+
+    }
+
+    @GetMapping()
+    public ResponseEntity<Iterable<ResourcesEntity>> findAllResources() {
+        Iterable<ResourcesEntity> allResources = resourcesService.findAllResources();
+        return new ResponseEntity<>(allResources, HttpStatus.OK);
+    }
 }
---
+++ src/main/java/se/iths/springbootproject/service/ResourcesService.java
@@ -0,0 +1,35 @@
+package se.iths.springbootproject.service;
+
+import org.springframework.stereotype.Service;
+import se.iths.springbootproject.entity.ResourcesEntity;
+import se.iths.springbootproject.repository.ResourcesRepo;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
+
+@Service
+public class ResourcesService {
+
+    private final ResourcesRepo resourcesRepo;
+
+    public ResourcesService(ResourcesRepo resourcesRepo) {
+        this.resourcesRepo = resourcesRepo;
+    }
+
+    public ResourcesEntity createResources (ResourcesEntity resourcesEntity){
+        return resourcesRepo.save(resourcesEntity);
+    }
+
+    public void deleteResources (Long resourcesId){
+        ResourcesEntity foundResources = resourcesRepo.findById(resourcesId).orElseThrow(EntityNotFoundException::new);
+        resourcesRepo.deleteById(foundResources.getResourcesid());
+    }
+
+    public Optional<ResourcesEntity> findResourcesById(Long resourcesId) {
+        return resourcesRepo.findById(resourcesId);
+    }
+
+    public Iterable<ResourcesEntity> findAllResources() {
+        return resourcesRepo.findAll();
+    }
+}
