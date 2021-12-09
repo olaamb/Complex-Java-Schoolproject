eea315eb4036e36857623e7ecd183b95d7c3dee2
Ola Amberntsson <ola.amberntsson@iths.se>
2021-12-09 10:49:45 +0100

    added view and templates

---
+++ src/main/java/se/iths/springbootproject/view/MvcConfig.java
@@ -0,0 +1,19 @@
+package se.iths.springbootproject.view;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/application").setViewName("application");
+        registry.addViewController("/admin").setViewName("admin");
+        registry.addViewController("/login").setViewName("login");
+
+    }
+
+}
